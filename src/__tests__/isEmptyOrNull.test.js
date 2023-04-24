001a9844a1ba0ba686b64fd297cb5648d80677ab
samuel.gomez <contact@samuel.gomez.fr>
2023-04-24 22:20:38 +0200

    feat(exo): solutions 2

--- src/__tests__/isEmptyOrNull.test.js
+++ src/__tests__/isEmptyOrNull.test.js
@@ -5,6 +5,21 @@ import isEmptyOrNull, {
 } from "../isEmptyOrNull";
 
 describe("isEmptyOrNull", () => {
+  /**
+   * Tester la fonction et vérifier les valeurs et résultats suivants
+    called with : null               => expected : true
+    called with : undefined          => expected : true
+    called with : ""                 => expected : true
+    called with : {}                 => expected : true
+    called with : []                 => expected : true
+    called with : "5"                => expected : false
+    called with : { test: "test" }   => expected : false
+    called with : 9                  => expected : false
+    called with : ["9.99"]           => expected : false
+    called with : new Blob(["blob"]) => expected : false
+    called with : new Blob()         => expected : false
+   * Méthodes de Jest à utiliser : each, toEqual
+   */
   test.each`
     elt                   | expected
     ${null}               | ${true}
@@ -24,6 +39,13 @@ describe("isEmptyOrNull", () => {
   });
 });
 
+/**
+   * Tester la fonction et vérifier les valeurs et résultats suivants
+    called with : {}                 => expected : true
+    called with : { test: "test" }   => expected : false
+    called with : new Blob(["blob"]) => expected : false
+   * Méthodes de Jest à utiliser : each, toEqual
+   */
 describe("isEmptyObject", () => {
   test.each`
     elt                   | expected
@@ -36,6 +58,12 @@ describe("isEmptyObject", () => {
   });
 });
 
+/**
+   * Tester la fonction et vérifier les valeurs et résultats suivants
+    called with : []       => expected : true
+    called with : ["9.99"] => expected : false
+   * Méthodes de Jest à utiliser : each, toEqual
+   */
 describe("isEmptyArray", () => {
   test.each`
     elt         | expected
@@ -47,6 +75,13 @@ describe("isEmptyArray", () => {
   });
 });
 
+/**
+   * Tester la fonction et vérifier les valeurs et résultats suivants
+    called with : null      => expected : true
+    called with : undefined => expected : true
+    called with : "test"    => expected : false
+   * Méthodes de Jest à utiliser : each, toEqual
+   */
 describe("isNullOrUndefined", () => {
   test.each`
     elt          | expected
--- src/__tests__/setAnomalyEmptyItems.test.js
+++ src/__tests__/setAnomalyEmptyItems.test.js
@@ -1,12 +1,25 @@
 import setAnomalyEmptyItems from "../setAnomalyEmptyItems";
 
 describe("setAnomalyEmptyItems", () => {
-  it("Should return null when receive items not empty", () => {
+  /**
+   * Tester si la valeur retournée est null avec en entrée ["item"]
+   * Méthodes de Jest à utiliser : toBeNull
+   */
+  test("Should return null when receive items not empty", () => {
     const result = setAnomalyEmptyItems(["item"]);
     expect(result).toBeNull();
   });
 
-  it("Should  when receive items not empty", () => {
+  /**
+   * Tester l'appel de la fonction sans paramètre si la valeur retournée est égale à :
+   * {
+      label: "Info : Aucune donnée trouvée",
+      type: "info",
+      iconName: "exclamation-sign",
+    }
+   * Méthodes de Jest à utiliser : toEqual
+   */
+  test("Should when receive items not empty", () => {
     const result = setAnomalyEmptyItems();
     const expected = {
       label: "Info : Aucune donnée trouvée",
